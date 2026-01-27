b342398c98b7e01e5d76d50edee56e1d0dbb5a61
archbober <git@archbober.com>
2026-01-27 15:13:43 +0100

    ref

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,27 +1,41 @@
-from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
 import string
+
+from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
 
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
-
-    preprocessed_query = preprocess_text(query)
-
     for movie in movies:
-        preprocessed_title = preprocess_text(movie["title"])
-        if len(results) >= limit:
-            break
-        for token in preprocessed_query:
-            if token in preprocessed_title:
-                results.append(movie)
+        query_tokens = tokenize_text(query)
+        title_tokens = tokenize_text(movie["title"])
+        if has_matching_token(query_tokens, title_tokens):
+            results.append(movie)
+            if len(results) >= limit:
                 break
 
     return results
 
-def preprocess_text(text: str) -> List(str):
+
+def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
+    for query_token in query_tokens:
+        for title_token in title_tokens:
+            if query_token in title_token:
+                return True
+    return False
+
+
+def preprocess_text(text: str) -> str:
     text = text.lower()
-    translator = str.maketrans('', '', string.punctuation)
-    text = text.translate(translator)
-    text_tokens = " ".join(text.split()).split(" ")
-    return text_tokens
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+
+def tokenize_text(text: str) -> list[str]:
+    text = preprocess_text(text)
+    tokens = text.split()
+    valid_tokens = []
+    for token in tokens:
+        if token:
+            valid_tokens.append(token)
+    return valid_tokens
